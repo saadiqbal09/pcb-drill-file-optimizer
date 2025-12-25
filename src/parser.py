df396ca294e458d1700309a7ae363491f0c419ee
saadiqbal09 <saadchavhan2@gmail.com>
2025-12-25 17:19:57 +0530

    Add basic drill file parsing for tools and coordinates

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,20 @@
 def parse_drill_file(path):
+    tools = {}
+    coordinates = []
+
     with open(path, "r") as f:
-        lines = f.readlines()
-    return lines
+        for line in f:
+            line = line.strip()
+
+            # Tool definition (example: T01C0.8)
+            if line.startswith("T") and "C" in line:
+                tool, size = line.split("C")
+                tools[tool] = float(size)
+
+            # Coordinate line (example: X012345Y067890)
+            elif line.startswith("X") and "Y" in line:
+                x_part = line.split("Y")[0][1:]
+                y_part = line.split("Y")[1]
+                coordinates.append((x_part, y_part))
+
+    return tools, coordinates
