c7c529209633b07d8fe7fcbb2ed627d404102f7f
saadiqbal09 <saadchavhan2@gmail.com>
2025-12-27 17:45:07 +0530

    Refactor drill file parser to use DrillPoint class

--- src/parser.py
+++ src/parser.py
@@ -1,20 +1,31 @@
+from dataclasses import dataclass
+
+@dataclass
+class DrillPoint:
+    x: float
+    y: float
+    tool: str
+
+
 def parse_drill_file(path):
     tools = {}
-    coordinates = []
+    points = []
+    current_tool = None
 
     with open(path, "r") as f:
         for line in f:
             line = line.strip()
 
-            # Tool definition (example: T01C0.8)
             if line.startswith("T") and "C" in line:
                 tool, size = line.split("C")
                 tools[tool] = float(size)
 
-            # Coordinate line (example: X012345Y067890)
-            elif line.startswith("X") and "Y" in line:
-                x_part = line.split("Y")[0][1:]
-                y_part = line.split("Y")[1]
-                coordinates.append((x_part, y_part))
+            elif line.startswith("T") and "C" not in line:
+                current_tool = line
 
-    return tools, coordinates
+            elif line.startswith("X") and "Y" in line:
+                x = float(line.split("Y")[0][1:]) / 1000
+                y = float(line.split("Y")[1]) / 1000
+                points.append(DrillPoint(x, y, current_tool))
+
+    return tools, points
